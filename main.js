a060cbfed3b8c7eddc712fce45c7b700f3a47987
Daniel Ors <do7811@gmail.com>
2019-03-09 02:47:59 -0500

    completed all functionality

--- main.js
+++ main.js
@@ -10,11 +10,11 @@
 // Data store for all details
 
 const STORE = {
-  price: [],
-  tax: [],
-  tip: [],
-  earnings: [],
-  mealCounter: [ 0 ],
+  price: 0,
+  tax: 0,
+  tip: 0,
+  earnings: 0,
+  mealCounter: 0,
   tipTotal: []
 };
 
@@ -23,13 +23,24 @@ const STORE = {
 function resetAll() {
   $('.js-reset-button').on( 'click', function (event) {
     event.preventDefault();
-    STORE.price = [];
-    STORE.tax = [];
-    STORE.tip = [];
-    STORE.earnings = [];
-    STORE.mealCounter = [ 0 ];
+    
+    STORE.price = 0;
+    STORE.tax = 0;
+    STORE.tip = 0;
+    STORE.earnings = 0;
+    STORE.mealCounter = 0;
     STORE.tipTotal = [];
+    
     $('.js-meal-entry-form')[0].reset();
+
+    $('.customer-charges-subtotal').html('');
+    $('.customer-charges-tip').html('');
+    $('.customer-charges-total').html('Enter a meal to see your charges.');
+
+    $('.earnings-info-tip-total').html('');
+    $('.earnings-meal-count').html('');
+    $('.earnings-average-tip').html('Enter a meal to see your total earnings.');
+
   });
 }
 
@@ -44,39 +55,30 @@ function cancelButton() {
 
 // handling price
 
-function handlePrice() {
-  $('.js-price').on('keypress', function() {
-    parseFloat($(this).val()).toFixed(2);
-    STORE.price[0] = $(this).val();
-    console.log(`Meal price is $${STORE.price[0]}`);
-  });
+function handlePrice(price) {
+  STORE.price = price;
+  console.log(`Meal price is $${STORE.price}`);
 }
 
 // handling tax
 
-function handleTax() {
-  $('.js-tax').on('keypress', function() {
-    parseFloat($(this).val()).toFixed(2);
-    STORE.tax[0] = $(this).val();
-    const taxInDollars = (( STORE.tax[0] * 0.01 ) * ( STORE.price[0] ));
-    console.log(taxInDollars);
-    STORE.tax[0] = taxInDollars;
-    console.log(`Meal tax is $${STORE.tax[0]}`);
-  });
+function handleTax(tax) {
+  STORE.tax = tax;
+
+  const taxInDollars = (( STORE.tax * 0.01 ) * ( STORE.price ));
+  
+  STORE.tax = taxInDollars;
+
+  console.log(`Meal tax is $${STORE.tax}`);
 }
 
 // putting together tip percentage
 
-function handleTip() {
-  $('.js-tip').on('keypress', function() {
-    parseFloat($(this).val()).toFixed(2);
-    STORE.tip[0] = $(this).val();
-    const tipInDollars = (( STORE.tip[0] * 0.01 ) * ( STORE.price[0] ));
-    console.log(tipInDollars);
-    STORE.tip[0] = tipInDollars;
-    console.log(`Meal tip is $${STORE.tip[0]}`);
-    STORE.tipTotal.push(STORE.tip[0]);
-  });
+function handleTip(tip) {
+  const tipInDollars = (( tip * 0.01 ) * ( STORE.price ));
+  STORE.tip = tipInDollars;
+  console.log(`Meal tip is $${STORE.tip}`);
+  STORE.tipTotal.push(STORE.tip);
 }
 
 // Calculating meal totals
@@ -84,48 +86,58 @@ function handleTip() {
 function mealCalculator() {
   $('.js-meal-entry-form').on( 'submit', function (event) {
     event.preventDefault();
-    const mealTotal = ( STORE.price[0] + STORE.tax[0] + STORE.tip[0] );
+
+    const price = parseFloat($('.js-price').val()).toFixed(2);
+    handlePrice(price);
+    const tax = parseFloat($('.js-tax').val()).toFixed(2);
+    handleTax(tax);
+    const tip = parseFloat($('.js-tip').val()).toFixed(2);
+    handleTip(tip);
+
+    const mealTotal = ( STORE.price + STORE.tax + STORE.tip );
+    parseFloat(mealTotal).toFixed(2);
     console.log(`Meal total is $${mealTotal}`);
-    STORE.earnings = mealTotal;
+
+    STORE.earnings += mealTotal;
     STORE.mealCounter++;
-    console.log(STORE.mealCounter);
+    console.log(`We have served ${STORE.mealCounter} meals.`);
+
+    earningsInfo();
+    customerCharges();
   });
 }
 
 // rendering customer charges - meal subtotal, tip, and total amounts into HTML
 
 function customerCharges() {
-  const subTotal = STORE.price[0] + STORE.tax[0];
-  const tipForPrint = STORE.tip[0];
+  const subTotal = STORE.price + STORE.tax;
+  const tipForPrint = STORE.tip;
   const totalCustomerCharge = subTotal + tipForPrint;
-  $('.customer-charges-subtotal').html(`Subtotal: ${subTotal}`);
-  $('.customer-charges-tip').html(`Tip: ${tipForPrint}`);
-  $('.customer-charges-total').html(`Total: ${totalCustomerCharge}`);
+  // console.log(STORE);
+  $('.customer-charges-subtotal').html(`Subtotal: $${parseFloat(subTotal).toFixed(2)}`);
+  $('.customer-charges-tip').html(`Tip: $${parseFloat(tipForPrint).toFixed(2)}`);
+  $('.customer-charges-total').html(`Total: $${parseFloat(totalCustomerCharge).toFixed(2)}`);
 }
 
 // rendering earnings info - tip totals, meal count, average tip per meal into HTML
 
 function earningsInfo() {
-  const tipTotal = function calculateTipTotal() {
-    const reducer = ( sum, curr ) => sum + curr;
-    STORE.tipTotal[0] = STORE.tipTotal.reduce( reducer );
-  };
-  $('.earnings-info-tip-total').html(`Tip Total: ${STORE.tipTotal[0]}`);
+  let tipSum = 0;
+  for (let i = 0; i < STORE.tipTotal.length; i++) {
+    tipSum += STORE.tipTotal[i];
+  }
+  let tipAvg = 0;
+  tipAvg = tipSum / STORE.mealCounter;
+  $('.earnings-info-tip-total').html(`Tip Total: $${parseFloat(tipSum).toFixed(2)}`);
   $('.earnings-meal-count').html(`Meal Count: ${STORE.mealCounter}`);
-  const averageTip = ( tipTotal / STORE.mealCounter );
-  $('.earnings-average-tip').html(`Average Tip: ${averageTip}`);
+  $('.earnings-average-tip').html(`Average Tip: $${parseFloat(tipAvg).toFixed(2)}`);
 }
 
 // calling necessary task functions so calculator can run
 
 function main() {
   cancelButton();
-  handlePrice();
-  handleTax();
-  handleTip();
   mealCalculator();
-  customerCharges();
-  earningsInfo();
   resetAll();
 }
 
